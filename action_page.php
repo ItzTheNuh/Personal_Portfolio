c7d3abefbf5b7899c5211a5766243abb12a96986
ItzTheNuh <115341527+ItzTheNuh@users.noreply.github.com>
2022-10-08 22:19:55 +0530

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,10 +1,8 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['fname'];
-    if (empty($name)) {
-        echo "Name is empty";
-    } else {
-        echo $name;
-    }
-}
-?>
+<html>
+<body>
+Welcome <?php echo $_POST["fname lname"]; ?><br>
+You are a <?php echo $_POST["gender"]; ?>
+Your birthday comes at <?php echo $_POST["birthday"]; ?><br>
+And your age is <?php echo $_POST["age"]; ?><br>   
+</body>
+</html>
