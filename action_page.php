025ee4f92c6411ef90d08af0463ba1bd7a3061c1
ItzTheNuh <115341527+ItzTheNuh@users.noreply.github.com>
2022-10-08 22:46:03 +0530

    Delete action_page.php

--- action_page.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-Welcome <?php echo $_POST["fname lname"]; ?><br>
-You are a <?php echo $_POST["gender"]; ?>
-Your birthday comes at <?php echo $_POST["birthday"]; ?><br>
-And your age is <?php echo $_POST["age"]; ?><br>   
-</body>
-</html>
